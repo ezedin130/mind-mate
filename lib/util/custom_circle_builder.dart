79dcf70c75e4d864d8d3dd014df4483eb6f4bd3c
Ezedi <ezedinawol130@gmail.com>
2025-11-04 00:36:21 +0300

    new feature: custom circle builder that is used to build decorative circles

---
+++ lib/util/custom_circle_builder.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class CustomCircleBuilder extends StatelessWidget {
+  const CustomCircleBuilder({
+    super.key,
+    required this.size,
+  });
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
