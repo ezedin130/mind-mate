f695a807b81620aa4b3819b9bd26cca982cea031
Ezedi <ezedinawol130@gmail.com>
2025-11-04 00:37:07 +0300

    new feature: custom card builder that is used to build smooth cards to be used in home page

---
+++ lib/util/custom_card_builder.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomCardBuilder extends StatelessWidget {
+  const CustomCardBuilder({
+    super.key,
+    required this.title,
+    required this.color,
+  });
+
+  final String title;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: color.withOpacity(0.4),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: GoogleFonts.lato(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
