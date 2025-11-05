f7fc0c8f237400fc709a7612642d4a179e01174a
Ezedi <ezedinawol130@gmail.com>
2025-11-05 13:59:21 +0300

    feature: designed chat-bot page UI

--- lib/pages/chatbot/chat_bot_page.dart
+++ lib/pages/chatbot/chat_bot_page.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mindmate/util/custom_text_field.dart';
 
 class ChatBotPage extends StatelessWidget {
   const ChatBotPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller= TextEditingController();
     return Scaffold(
-      body: Center(
-        child: Text(
-          'ChatBot',
-          style: GoogleFonts.lato(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-        ),
-        ),
+      backgroundColor: const Color(0xFF1E4957),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E4957),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(),
+          Center(
+            child: Text(
+              'How can I help?',
+              style: GoogleFonts.lato(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            ),
+          ),
+          const SizedBox(height: 20.0,),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: CustomTextField(
+              hint: "Ask Anything",
+              obscure: false,
+              controller: controller,
+            ),
+          )
+        ],
       ),
     );
   }
