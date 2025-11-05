550f42c23675858e3fcd0f3e58cfb80835ebd2a1
Ezedi <ezedinawol130@gmail.com>
2025-11-05 13:39:38 +0300

    feature:  new page for the guided activity page

---
+++ lib/pages/activity/guided_activity_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GuidedActivityPage extends StatelessWidget {
+  const GuidedActivityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Guided Activity',
+          style: GoogleFonts.lato(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+        ),
+        ),
+      ),
+    );
+  }
+}
