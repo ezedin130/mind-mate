4247151bc04070fc3b867121a36000d9f7133f8a
Ezedi <ezedinawol130@gmail.com>
2025-11-05 13:38:41 +0300

    feature:  add onTap callback to CustomCardBuilder for tappable cards

--- lib/util/custom_card_builder.dart
+++ lib/util/custom_card_builder.dart
@@ -7,34 +7,39 @@ class CustomCardBuilder extends StatelessWidget {
     super.key,
     required this.title,
     required this.color,
+    required this.onTap
   });
 
   final String title;
   final Color color;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: color.withOpacity(0.4),
-            blurRadius: 10,
-            offset: const Offset(0, 5),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: color.withOpacity(0.4),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: GoogleFonts.lato(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            textAlign: TextAlign.center,
           ),
-        ],
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: GoogleFonts.lato(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-          textAlign: TextAlign.center,
         ),
       ),
     );
