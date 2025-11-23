65fe9342f788a65f4aba19e86ac76f3af3e2a56e
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-23 10:23:24 +0300

    feat: breathing exercise page

---
+++ lib/pages/activity/pages/breathing_exercise_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BreathingExercisePage extends StatelessWidget {
+  const BreathingExercisePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E4957),
+      ),
+      backgroundColor: const Color(0xFF1E4957),
+      body:  Center(
+        child: Text(
+          'Breathing Exercise',
+          style: GoogleFonts.lato(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
