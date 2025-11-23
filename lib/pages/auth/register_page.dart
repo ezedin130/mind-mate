e3c8334fd32dcfd50ee202f789d12f46ea6aaf68
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-23 17:37:13 +0300

    feat: add validation to text fields and authentication service

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mindmate/auth_service.dart';
 import 'package:mindmate/pages/auth/login_page.dart';
 import 'package:mindmate/util/custom_text_field.dart';
 
@@ -8,145 +9,219 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+    final authService = AuthService();
+    final nameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
     return Scaffold(
       backgroundColor: Color(0XFF088F8F),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'MindMate',
-            style: GoogleFonts.lato(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 30,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            'Mental Health Companion',
-            style: GoogleFonts.lato(color: Colors.white, fontSize: 15),
-          ),
-          const SizedBox(height: 10),
-          Center(
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              margin: const EdgeInsets.all(20.0),
-              width: 300,
-              height: 400,
-              decoration: BoxDecoration(
-                color: const Color(0xFF3E5A66),
-                borderRadius: BorderRadius.circular(25),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.25),
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'MindMate',
+              style: GoogleFonts.lato(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 30,
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomTextField(hint: 'Name', obscure: false,color: Colors.white,),
-                  const SizedBox(height: 15),
-                  CustomTextField(hint: 'Email', obscure: false,color: Colors.white,),
-                  const SizedBox(height: 15),
-                  CustomTextField(hint: 'Password', obscure: true,color: Colors.white,),
-                  const SizedBox(height: 15),
-                  CustomTextField(hint: 'Confirm Password', obscure: true,color: Colors.white,),
-                  const SizedBox(height: 25),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 45,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF4EB3E8),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Mental Health Companion',
+              style: GoogleFonts.lato(color: Colors.white, fontSize: 15),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                margin: const EdgeInsets.all(20.0),
+                width: 300,
+                height: 450,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF3E5A66),
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25),
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomTextField(
+                      hint: 'Name 4 characters at least',
+                      controller: nameController,
+                      obscure: false,
+                      color: Colors.white,
+                      validator: (value) {
+                        if (value == null || value.isEmpty || value.length < 4) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextField(
+                      hint: 'Email',
+                      controller: emailController,
+                      obscure: false,
+                      color: Colors.white,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+                        if (!RegExp(pattern).hasMatch(value)) {
+                          return 'Enter a valid email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextField(
+                      hint: 'Password 6 characters at least',
+                      controller: passwordController,
+                      obscure: true,
+                      color: Colors.white,
+                      validator: (value) {
+                        if (value == null || value.isEmpty || value.length < 6) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    CustomTextField(
+                      hint: 'Confirm Password',
+                      obscure: true,
+                      color: Colors.white,
+                      validator: (value) {
+                        if (value == null || value != passwordController.text) {
+                          return 'password does not match';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 25),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF4EB3E8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          elevation: 3,
                         ),
-                        elevation: 3,
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        'Regsiter',
-                        style: GoogleFonts.lato(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+                        onPressed: () async{
+                          if (formKey.currentState!.validate()) {
+                            String responseMessage = await authService.registerUser(
+                              nameController.text,
+                              emailController.text,
+                              passwordController.text,
+                            );
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text(responseMessage)),
+                            );
+
+                            if (responseMessage == "User Registered") {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LoginPage()),
+                              );
+                            }
+                          }
+                        },
+                        child: Text(
+                          'Register',
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(child: Divider(thickness: 0.5, color: Colors.grey[400])),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Text(
-                  'Or continue with',
-                  style: GoogleFonts.lato(
-                    color: Colors.grey[500],
-                    fontWeight: FontWeight.bold,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Divider(thickness: 0.5, color: Colors.grey[400])),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    'Or continue with',
+                    style: GoogleFonts.lato(
+                      color: Colors.grey[500],
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(child: Divider(thickness: 0.5, color: Colors.grey[400])),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Image.asset('lib/assets/google (2).png', width: 35),
-                  SizedBox(width: 10),
-                  Image.asset('lib/assets/insta.png', width: 35),
-                  SizedBox(width: 10),
-                  Image.asset('lib/assets/fb.png', width: 35),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Already have an account?',
-                style: GoogleFonts.lato(
-                  color: Colors.grey[700],
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+                Expanded(child: Divider(thickness: 0.5, color: Colors.grey[400])),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Image.asset('lib/assets/google (2).png', width: 35),
+                    SizedBox(width: 10),
+                    Image.asset('lib/assets/insta.png', width: 35),
+                    SizedBox(width: 10),
+                    Image.asset('lib/assets/fb.png', width: 35),
+                  ],
                 ),
-              ),
-              SizedBox(width: 5),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginPage(),
-                    ),
-                  );
-                },
-                child: Text(
-                  'Login now',
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already have an account?',
                   style: GoogleFonts.lato(
-                    color: Colors.blue,
+                    color: Colors.grey[700],
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(width: 5),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Login now',
+                    style: GoogleFonts.lato(
+                      color: Colors.blue,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
